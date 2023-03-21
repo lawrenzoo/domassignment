114970300e111225a5d2fa9877bd98807860497e
lawrenzoo <chiagozielawrence@gmail.com>
2023-03-21 17:54:54 +0100

    dom start

---
+++ dom.js
@@ -0,0 +1,8 @@
+function domwork(height, background, textcolor, width){
+
+    var myGod = document.getElementById("helpme");
+    myGod.style.height = height;
+    myGod.style.backgroundColor = background;
+    myGod.style.color = textcolor;
+    myGod.style.width = width;
+}
